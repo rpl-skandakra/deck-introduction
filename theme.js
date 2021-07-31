52efae6be4353ccd937381a463888b1bac98da38
hendraaagil <hendraaagil@gmail.com>
2021-07-31 11:15:06 +0700

    init: create project

---
+++ theme.js
@@ -0,0 +1,11 @@
+export default {
+  fonts: {
+    body: 'Poppins, sans-serif',
+    heading: 'Poppins, sans-serif',
+  },
+  colors: {
+    background: '#4285f4',
+    primary: '#3ddc84',
+    text: '#faf7fc',
+  },
+};
